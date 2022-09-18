6612e3ed9a578a08a320a07384fb865afabffacd
iitaellu <78799868+iitaellu@users.noreply.github.com>
2022-09-18 19:25:51 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,6 @@ submitButton.addEventListener("click", function () {
     var table = document.getElementById("userdata");
     var count = table.tBodies[0].rows.length;
     //Check if username is used already
-    var exists = 0;
     for (var r = 0; r < count; r++) {
       //help from https://www.w3schools.com/jsref/coll_table_rows.asp
       if (document.getElementById("data").rows[r].cells[0].innerHTML === user) {
@@ -36,24 +35,48 @@ submitButton.addEventListener("click", function () {
         x[1].innerHTML = email;
         x[2].innerHTML = address;
         x[3].innerHTML = admin;
-        exists = 1;
+        document.getElementById("input-username").value = "";
+        document.getElementById("input-email").value = "";
+        document.getElementById("input-address").value = "";
+        return;
+      } else {
+        newRow.append(newuser, newemail, newaddress, newadmin);
+        document.getElementById("data").appendChild(newRow);
         document.getElementById("input-username").value = "";
         document.getElementById("input-email").value = "";
         document.getElementById("input-address").value = "";
       }
     }
-    if (exists === 0) {
-      newRow.append(newuser, newemail, newaddress, newadmin);
-      document.getElementById("data").appendChild(newRow);
-      document.getElementById("input-username").value = "";
-      document.getElementById("input-email").value = "";
-      document.getElementById("input-address").value = "";
-    }
   }
 });
 
+//Clear Table
 const emptyButton = document.getElementById("empty-table"); //help from https://stackoverflow.com/questions/7271490/delete-all-rows-in-an-html-table
 emptyButton.addEventListener("click", function () {
   var ntable = document.getElementById("data");
   ntable.innerHTML = "";
+});
+
+//Add image
+const addImage = document.getElementById("upload");
+addImage.addEventListener("click", function () {
+  var user = document.getElementById("input-username").value;
+  var table = document.getElementById("userdata");
+  var count = table.tBodies[0].rows.length;
+
+  //make url for picture
+  const file = document.getElementById("input-image").files[0];
+  let src = URL.createObjectURL(file);
+  var img = document.createElement("img");
+  img.setAttribute("src", src); //Help from https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_img_create
+  img.setAttribute("width", "64");
+  img.setAttribute("height", "64");
+  console.log(img);
+
+  for (var r = 0; r < count; r++) {
+    if (document.getElementById("data").rows[r].cells[0].innerHTML === user) {
+      document.getElementById("data").rows[r].insertCell(-1).appendChild(img);
+      return;
+    }
+  }
 });
